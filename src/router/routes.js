851c84c5ef527a73f15c78c4e432a5892c7b7d0c
Maitan <victormaitan01@gmail.com>
2022-10-24 00:55:13 -0400

    🔧 CONFIG

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,14 +6,14 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
-  },
+  }
 
   // Always leave this as last one,
   // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+  // {
+  //   path: '/:catchAll(.*)*',
+  //   component: () => import('pages/ErrorNotFound.vue')
+  // }
 ]
 
 export default routes
